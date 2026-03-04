b5bd774c7f574f740661086ad3d0c7f2635a3208
swager6545 <60736546+swager6545@users.noreply.github.com>
2026-03-04 14:21:06 +0000

    Added a health component

---
+++ Source/FPSTemplate/HealthComponent.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	MaxHealth = 100.0f;
+	Health = MaxHealth;
+}
+
+
+void UHealthComponent::UpdateHealth(float Damage, AActor* DamageCauser)
+{
+	Health -= Damage;
+	//clamp the values so it won't go over or under the health and max health values
+	Health = FMath::Clamp(Health, 0.f, MaxHealth);
+	
+	//updates the health value
+	OnHealthChanged.Broadcast(Health, MaxHealth);
+	
+	//update the damage when the actor gets hit
+	if (Health == 0)
+	{
+		OnDeath.Broadcast(DamageCauser);
+	}
+	
+	
+	
+}
+
---
+++ Source/FPSTemplate/HealthComponent.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDamageChanged, AActor*, Hitter);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChanged, float, NewHealth, float, MaxHealth);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class FPSTEMPLATE_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+	//current health
+	UPROPERTY()
+	float Health;
+	
+	//max health
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	float MaxHealth;
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+	
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnHealthChanged OnHealthChanged;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnDamageChanged OnDeath;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void UpdateHealth(float Damage, AActor* DamageCauser);
+	
+
+		
+};
